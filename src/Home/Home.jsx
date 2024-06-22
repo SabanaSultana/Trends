1358d38f65d9c789ef0a0a2053c7a4eb13c31dfa
Sabana <sabanasultana132@gmail.com>
2024-06-22 10:09:11 +0530

    initial commit

---
+++ src/Home/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <div className='home'>
+        <div className="heading">
+            <div>NEW ARRIVALS ONLY</div>
+            <div>
+                <span>new</span>
+                <img src="" alt="Error Loading image" />
+            </div>
+            <div>
+                collections
+            </div>
+            <div>
+                for everyone
+            </div>
+            <div>
+                <button>Men</button>
+                <button>Women</button>
+            </div>
+        </div>
+        <div className="images">
+            <img src="" alt="error loading image" />
+        </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/Navigation Menu/Navs.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaShoppingCart } from 'react-icons/fa';
+import './Navs.css';
+
+function Navs() {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        Trendy Threads
+      </div>
+      <div className="navs">
+        <ul>
+          <li>
+            <Link to="/"><span >Home</span></Link>
+          </li>
+          <li>
+            <Link to="/about"><span class="col-nav">About</span></Link>
+          </li>
+          <li>
+            <Link to="/contact"><span >Contact Us</span></Link>
+          </li>
+        </ul>
+      </div>
+      <div className="login">
+        <div className='login-signup'>
+          <Link to="/login">
+                <button className='login-signup'>SignUp/Login</button>
+          </Link>
+        </div>
+        <div>
+          <Link to="/">
+            <FaShoppingCart className="cart-icon" />
+          </Link>
+        </div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default Navs;
